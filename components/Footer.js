8b385f3968bad6bfce981931e8a5213e2d358729
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-06 07:44:37 +0100

    Footer Section Adjusted

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 px-16 md:px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 px-8 md:px-32 py-14 bg-gray-100 text-gray-600">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-black">ABOUT</h5>
 
@@ -14,31 +14,31 @@ function Footer() {
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-black">COMMUNITY</h5>
 
-                <p>Accesibility</p>
-                <p>This is not a real site</p>
-                <p>Its a pretty awesome clone</p>
-                <p>Referrals accepted</p>
-                <p>Papafam</p>
+                <p>Accessibility</p>
+                <p>Airbnb Associates</p>
+                <p>Guest Referrals</p>
+                <p>Gift cards</p>
+                <p>Airbnb.org</p>
             </div>
 
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-black">HOST</h5>
 
-                <p>Papa React</p>
-                <p>Presents</p>
-                <p>Zero to Full Stack Hero</p>
-                <p>Hundreds of Students</p>
-                <p>Join Now</p>
+                <p>Host your home</p>
+                <p>Host an Online Experience</p>
+                <p>Host an Experience</p>
+                <p>Responsible hosting</p>
+                <p>Resource Center</p>
             </div>
 
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-black">SUPPORT</h5>
 
-                <p>Help Centre</p>
+                <p>Our COVID-19 Response</p>
+                <p>Help Center</p>
+                <p>Cancellation options</p>
+                <p>Neighborhood Support</p>
                 <p>Trust & Safety</p>
-                <p>Say Hi YouTube</p>
-                <p>Easter Eggs</p>
-                <p>For the Win</p>
             </div>
         </div>
     )
--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function LargeCard({ img, title, description, buttonText }) {
     return (
-        <div className="relative py-16">
+        <div className="relative my-16">
             <div className="relative h-96">
                 <Image 
                     src={img}
@@ -12,8 +12,8 @@ function LargeCard({ img, title, description, buttonText }) {
                 />
             </div>
 
-            <div className="absolute top-32 left-12">
-                <h3 className="text-4xl mb-3 w-64">{title}</h3>
+            <div className="absolute top-16 left-1/2 transform -translate-x-1/2 sm:translate-x-0 sm:left-12">
+                <h3 className="text-4xl mb-3 w-56">{title}</h3>
                 <p>{description}</p>
 
                 <button className="text-sm bg-gray-900 text-white py-2 px-4 rounded-lg mt-5 cursor-pointer">{buttonText}</button>
